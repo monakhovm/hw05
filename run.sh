d75e4227aad5b850e4bf90ae1d09f1a68c6d2fb2
monakhovm <monakhov.m@gmail.com>
2023-09-05 19:02:32 +0300

    Main fixes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ source .env
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-BLUE='\033[0;34m'
+BLUE='\033[0;94m'
 NC='\033[0m' # No Color
 
 check_env_vars() {
@@ -19,58 +19,72 @@ check_env_vars() {
 check_python_version() {
     if [ -f $(command -v python3) ]; then
         python=$(command -v python3);
-    
         installed_python_version=$($python -V | grep -oP '\d+\.\d+')
-
-        installed_python_major_version=$(echo $installed_python_version | cut -d '.' -f 1)
-        installed_python_minor_version=$(echo $installed_python_version | cut -d '.' -f 2)
         true
     else
         echo -e "${RED}Python is not intalled on this system${NC}"
         false
     fi
-
 }
 
-set_package_manager_and_packages() {
+get_package_manager() {
+    if command -v apt &>/dev/null; then
+        package_manager="apt"
+
+    elif command -v dnf &>/dev/null; then
+        package_manager="dnf"
+
+    elif command -v yum &>/dev/null; then
+        package_manager="yum"
+    
+    else
+        echo -e "${RED}Unsupported package manager${NC}"
+        echo -e "Please resolve needed dependencies:"
+        echo -e "\t- Python 3.9 and later"
+        echo -e "\t- python3-pip"
+        echo -e "\t- git"
+        echo -e "\t- gcc"
+        echo -e "\t- sed"
+        echo -e "\t- libpq-dev"
+        echo -e "\t- python-dev\n"
+        echo -e "And then run this script with '--no-install' parameter"
+        exit 1
+    fi
+}
+
+update_cache() {
+    # Update package list
+    if [ "$package_manager" == "apt" ]; then
+        sudo $package_manager update
+    elif [ "$package_manager" == "dnf" ] || [ "$package_manager" == "yum" ]; then
+        sudo $package_manager check-update
+    fi
+}
+
+set_packages() {
     install_command="install"
     parameter="-y"
     postgresql_lib=postgresql-devel
 
-    if command -v apt &>/dev/null; then
-        package_manager="apt"
+
+    if $package_manager="apt"; then        
         available_python_version=$(apt-cache madison python3 | grep -oP '\d+\.\d+' | head -n 1)
         devel_package="python${available_python_version}-dev"
         postgresql_lib=libpq-dev
 
-    elif command -v dnf &>/dev/null; then
-        package_manager="dnf"
-        available_python_version=$(dnf list python3 | grep -oP '\d+\.\d+' | head -n 1)
-        devel_package="python${available_python_version}-devel"
-
-    elif command -v yum &>/dev/null; then
-        package_manager="yum"
-        available_python_version=$(yum list python3 | grep -oP '\d+\.\d+' | head -n 1)
-        devel_package="python${available_python_version}-devel"
-
     else
-        if [ "$1" != "--no-install" ];  then
-            echo -e "${RED}Unsupported package manager${NC}"
-            echo -e "Please resolve needed dependencies:"
-            echo -e "\t- Python 3.9 and later"
-            echo -e "\t- python3-pip"
-            echo -e "\t- git"
-            echo -e "\t- gcc"
-            echo -e "\t- sed"
-            echo -e "\t- libpq-dev"
-            echo -e "\t- python-dev\n"
-            echo -e "And then run this script with '--no-install' parameter"
-            exit 1
-        fi
+        available_python_version=$($package_manager list python3 | grep 'python3' | grep -oP '\d+\.\d+' | head -n 1)
+        devel_package="python${available_python_version}-devel"
     fi
 
-    if [ "$installed_python_major_version" -lt 3 ] || { [ "$installed_python_major_version" -eq 3 ] && [ "$installed_python_minor_version" -lt 9 ]; }; then
-        if [ "$available_python_major_version" -lt 3 ] || { [ "$available_python_major_version" -eq 3 ] && [ "$available_python_minor_version" -lt 9 ]; }; then
+    installed_python_major_version=$(echo $installed_python_version | cut -d '.' -f 1)
+    installed_python_minor_version=$(echo $installed_python_version | cut -d '.' -f 2)
+
+    $available_python_major_version=$(echo $available_python_version | cut -d '.' -f 1)
+    $available_python_minor_version=$(echo $available_python_version | cut -d '.' -f 2)
+
+    if [[ "$installed_python_major_version$installed_python_minor_version" -lt 39 ]]; then
+        if [[ "$available_python_major_version$available_python_minor_version" -lt 39 ]]; then
             echo -e "${RED}Python 3.9 or later is required. Only Python $available_python_version is available in the repository. Please install Python 3.9 or later manually.${NC}"
             exit 1
         else
@@ -78,20 +92,6 @@ set_package_manager_and_packages() {
         fi
     else
         common_packages=("python3-pip" "git" "sed" "gcc" $postgresql_lib $devel_package)
-    fi
-}
-
-install_packages() {
-    if [ "$1" == "--no-install" ]; then
-        echo -e "${GREEN}Skipping package installation as per '--no-install' parameter.${NC}"
-        return
-    fi
-
-    # Update package list
-    if [ "$package_manager" == "apt" ]; then
-        sudo $package_manager update
-    elif [ "$package_manager" == "dnf" ] || [ "$package_manager" == "yum" ]; then
-        sudo $package_manager check-update
     fi
 
     # Initialize packages array
@@ -109,24 +109,39 @@ install_packages() {
             fi
         fi
     done
+}
 
-    # If there are any packages to install, build the command and run it
-    if [ ${#packages[@]} -gt 0 ]; then
-        # Build the command as a single string
-        full_command="sudo $package_manager $install_command $parameter ${packages[*]}"
+install_packages() {
+    echo -e "Application needs some dependecies. They are: $packages\nDo you want to install it?"
+    while true; do
+        read -p "Enter your choice (y/Y for 'yes' and n/N for 'no'): " choice
+        case $choice in
+            [Yy]* ) answer="YES"; break;;
+            [Nn]* ) answer="NO"; break;;
+            * ) echo "Please answer y/Y/n/N.";;
+        esac
+    done
+    if [ $answer -eq "YES" ]; then
+        # If there are any packages to install, build the command and run it
+        if [ ${#packages[@]} -gt 0 ]; then
+            # Build the command as a single string
+            full_command="sudo $package_manager $install_command $parameter ${packages[*]}"
 
-        echo -e "${BLUE}Installing required packages: ${packages[*]}${NC}"
-        eval $full_command
+            echo -e "${BLUE}Installing required packages: ${packages[*]}${NC}"
+            eval $full_command
 
-        # Check that the command completed successfully
-        if [ $? -ne 0 ]; then
-            echo -e "${RED}Package installation failed. Please check the output above for more details.${NC}"
-            exit 1
+            # Check that the command completed successfully
+            if [ $? -ne 0 ]; then
+                echo -e "${RED}Package installation failed. Please check the output above for more details.${NC}"
+                exit 1
+            else
+                echo -e "${GREEN}All required packages installed successfully.${NC}"
+            fi
         else
-            echo -e "${GREEN}All required packages installed successfully.${NC}"
+            echo -e "${GREEN}All required packages are already installed.${NC}"
         fi
     else
-        echo -e "${GREEN}All required packages are already installed.${NC}"
+        exit 1
     fi
 }
 
@@ -174,9 +189,11 @@ run_django_server() {
 main() {
     check_env_vars
     check_python_version
-    
+    get_package_manager
+
     if [ "$1" != "--no-install" ]; then
-        set_package_manager_and_packages
+        update_cache
+        set_packages
         install_packages
     fi
 
